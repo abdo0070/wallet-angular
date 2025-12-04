45ea08f837cd7579572ae510f4a6552b1e9a598b
unknown <abdallammdouh6@gmail.com>
2025-12-04 20:58:30 +0200

    add the basic compnent

---
+++ src/app/components/goal-setting/goal-setting.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-goal-setting',
+  standalone: true,
+  imports: [],
+  templateUrl: './goal-setting.component.html',
+  styleUrl: './goal-setting.component.css'
+})
+export class GoalSettingComponent {
+
+}
