ed07377e9160eff9a7cd829d27bf301882fbb33d
unknown <abdallammdouh6@gmail.com>
2025-12-07 16:30:04 +0200

    add the goal setting html component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { DashboardHeaderComponent } from "./components/dashboard-header/dashboard-header.component";
+import { GoalSettingComponent } from "./components/goal-setting/goal-setting.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DashboardComponent, DashboardHeaderComponent],
+  imports: [RouterOutlet, DashboardComponent, DashboardHeaderComponent, GoalSettingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
