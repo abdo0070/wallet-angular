7015fe7bd0575d7b96173c053ea0b826cd748ade
unknown <abdallammdouh6@gmail.com>
2025-12-07 16:17:30 +0200

    make the dashboard & dashboard-header component

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DashboardHeaderComponent } from "../dashboard-header/dashboard-header.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [DashboardHeaderComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
